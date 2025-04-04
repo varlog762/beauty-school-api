2166ea409057da66da8a52b2e9c773e89f7cd308
Gregory Pereverzev <boot762@gmail.com>
2025-04-04 16:05:06 +0300

    feat: add 'register' route

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,3 +1,19 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
 const router = Router();
+
+router.post('/', (req, res) => {
+  const { course, name, phone, date } = req.body;
+
+  if (!name || !course || !phone || !date) {
+    return res
+      .status(400)
+      .json({ error: 'All fields are required: course, name, phone, date' });
+  }
+
+  console.log(course, name, phone, date);
+
+  return res.status(201).json({ status: 'success' });
+});
+
+export default router;
