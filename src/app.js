94ed4830234c4b9f0efd04874656fcd46a33e345
Gregory Pereverzev <boot762@gmail.com>
2025-04-04 16:08:47 +0300

    install dotenv

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import express from 'express';
+import registerRouter from './routes/register.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app;
