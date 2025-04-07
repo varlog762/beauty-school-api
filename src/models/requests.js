66a40d7c0fec6028e9f543a33a716d1bc82ad558
Gregory Pereverzev <boot762@gmail.com>
2025-04-07 22:16:08 +0300

    update models/requests.js

--- src/models/requests.js
+++ src/models/requests.js
@@ -1,9 +1,11 @@
 import db from '../db/database.js';
 
 export const saveClientRequestToDB = (name, phone, course) => {
-  db.prepare('INSERT INTO requests (name, phone, course) VALUES (?, ?, ?)').run(
-    name,
-    phone,
-    course
-  );
+  try {
+    db.prepare(
+      'INSERT INTO requests (name, phone, course) VALUES (?, ?, ?)'
+    ).run(name, phone, course);
+  } catch (error) {
+    console.error('Ошибка при сохранении заявки:', error.message);
+  }
 };
